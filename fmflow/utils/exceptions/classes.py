532d1b00a29dd163b1d0ab9abcb3cb9cc375ea80
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-04-01 00:46:25 +0900

    Add class 'FMFlowError'

--- fmflow/utils/exceptions/classes.py
+++ fmflow/utils/exceptions/classes.py
@@ -1,1 +1,18 @@
 # coding: utf-8
+
+# imported items
+__all__ = ['FMFlowError']
+
+
+# classes
+class FMFlowError(Exception):
+    """Error class of FMFlow.
+
+    """
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
+
+
