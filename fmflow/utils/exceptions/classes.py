e066a749d7fd01ec746ccf4ed5df2283d62043b9
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-04-01 00:46:54 +0900

    Add class 'FMFlowWarning'

--- fmflow/utils/exceptions/classes.py
+++ fmflow/utils/exceptions/classes.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 # imported items
-__all__ = ['FMFlowError']
+__all__ = ['FMFlowError', 'FMFlowWarning']
 
 
 # classes
@@ -16,3 +16,12 @@ class FMFlowError(Exception):
         return repr(self.message)
 
 
+class FMFlowWarning(Warning):
+    """Warning class of FMFlow.
+
+    """
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
