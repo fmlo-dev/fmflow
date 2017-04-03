b41b36c29a3be8b2e4f7c22068919adfef468797
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-04-03 21:47:15 +0900

    Add import 'progress'

--- fmflow/utils/__init__.py
+++ fmflow/utils/__init__.py
@@ -5,7 +5,9 @@ from .datetime import *
 from .exceptions import *
 from .multiprocessing import *
 from .ndarray import *
+from .progress import *
 del datetime
 del exceptions
 del multiprocessing
 del ndarray
+del progress
