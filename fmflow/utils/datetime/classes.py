db2b8c6319d8d5b6ff1e076880b06b499ed8744e
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-04-03 21:23:14 +0900

    Add class 'DatetimeParser'

--- fmflow/utils/datetime/classes.py
+++ fmflow/utils/datetime/classes.py
@@ -1,1 +1,62 @@
 # coding: utf-8
+
+# imported items
+__all__ = ['DatetimeParser']
+
+# standard library
+from datetime import datetime, timedelta
+
+# constants
+ISO_8601 = '%Y-%m-%dT%H:%M:%S.%f'
+PATTERNS = [
+    '%y%m%d%H%M%S',
+    '%y%m%d%H%M%S.',
+    '%y%m%d%H%M%S.%f',
+    '%Y%m%d%H%M%S',
+    '%Y%m%d%H%M%S.',
+    '%Y%m%d%H%M%S.%f',
+]
+
+
+# classes
+class DatetimeParser(object):
+    def __init__(self, cutoffsec=True):
+        """Initialize a datetime parser object.
+
+        Args:
+            cutoffsec (bool, optional): If True, digits smaller than 0.1 second is
+                truncated. For example, 0.123 sec becomes 0.100 sec. Default is True.
+
+        """
+        self.cutoffsec = cutoffsec
+        self.pattern = None
+
+    def __call__(self, string):
+        """Convert a datetime string to that in ISO format.
+
+        Args:
+            string (str): A datetime string.
+
+        Returns:
+            isostring (str): A datetime string in ISO format.
+
+        """
+        try:
+            dt = datetime.strptime(string, self.pattern)
+        except:
+            self._setpattern(string)
+            dt = datetime.strptime(string, self.pattern)
+
+        if self.cutoffsec:
+            return dt.strftime(ISO_8601)[:-5] + '00000'
+        else:
+            return dt.strftime(ISO_8601)
+
+    def _setpattern(self, string):
+        for pattern in PATTERNS:
+            try:
+                dt = datetime.strptime(string, pattern)
+                self.pattern = pattern
+                break
+            except:
+                continue
