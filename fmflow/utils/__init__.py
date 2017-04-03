334820095a6d00706fe9086fd681f53bbbf4508a
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-04-03 21:23:34 +0900

    Add import 'datetime'

--- fmflow/utils/__init__.py
+++ fmflow/utils/__init__.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 
 # submodules
+from .datetime import *
 from .exceptions import *
 from .multiprocessing import *
 from .ndarray import *
+del datetime
 del exceptions
 del multiprocessing
 del ndarray
