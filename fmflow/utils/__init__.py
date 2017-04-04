4345fc63128b52a21b1a0e33d43b90c594c76352
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-04-04 21:46:38 +0900

    Add import 'binary'

--- fmflow/utils/__init__.py
+++ fmflow/utils/__init__.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 
 # submodules
+from .binary import *
 from .datetime import *
 from .exceptions import *
 from .multiprocessing import *
 from .ndarray import *
 from .progress import *
+del binary
 del datetime
 del exceptions
 del multiprocessing
