5011ec4293569aefc45bbd35e07e5f641ce2e563
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-04-06 09:59:13 +0900

    Add function 'ctype_to_tform'

--- fmflow/utils/fits/functions.py
+++ fmflow/utils/fits/functions.py
@@ -1,1 +1,47 @@
 # coding: utf-8
+
+# imported items
+__all__ = ['ctype_to_tform']
+
+# standard library
+import re
+
+
+# functions
+def ctype_to_tform(ctype):
+    """Convert Python C-type to FITS format.
+
+    Args:
+        ctype (str): A C-type format string in Python.
+
+    Returns:
+        tform (str): A format string of FITS (TFORM).
+
+    References
+        https://docs.python.org/3.6/library/struct.html
+        http://docs.astropy.org/en/stable/io/fits/usage/table.html
+
+    """
+    # character
+    if re.search('s', ctype):
+        return 'A{}'.format(re.findall('\d+', ctype)[0])
+    # unsigned byte
+    elif re.search('B', ctype):
+        return 'B'
+    # 32-bit integer
+    elif re.search('i', ctype):
+        return 'J'
+    # 64-bit integer
+    elif re.search('q', ctype):
+        return 'K'
+    # single precision floating point
+    elif re.search('f', ctype):
+        return 'E'
+    # double precision floating point
+    elif re.search('d', ctype):
+        return 'D'
+    # otherwise
+    else:
+        raise ValueError(ctype)
+
+
