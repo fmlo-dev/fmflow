b116310473c66a8308a371fa813f9156da825a07
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-04-09 17:17:08 +0900

    Update class 'DatetimeParser'

--- fmflow/utils/datetime/classes.py
+++ fmflow/utils/datetime/classes.py
@@ -4,7 +4,7 @@
 __all__ = ['DatetimeParser']
 
 # standard library
-from datetime import datetime, timedelta
+from datetime import datetime
 
 # dependent packages
 import numpy as np
@@ -23,49 +23,50 @@ PATTERNS = [
 
 # classes
 class DatetimeParser(object):
-    def __init__(self, cutoffsec=True):
+    def __init__(self, cutoffsec=True, encoding='utf-8'):
         """Initialize a datetime parser object.
 
         Args:
             cutoffsec (bool, optional): If True, digits smaller than 0.1 second is
                 truncated. For example, 0.123 sec becomes 0.100 sec. Default is True.
+            encoding (str, optional): An encoding with which to decode datetime strings
+                if their type is bytes. Default is utf-8.
 
         """
-        self.cutoffsec = cutoffsec
-        self.pattern = None
+        self.info = {'cutoffsec': cutoffsec, 'encoding': encoding}
+        self._pattern = None
 
-    def __call__(self, string, encoding='utf-8'):
+    def __call__(self, dt_string):
         """Convert a datetime string to that in ISO format.
 
         Args:
-            string (str): A datetime string.
-            encoding (str, optional): An encoding with which to decode `string` if it is bytes.
+            dt_string (str): A datetime string.
 
         Returns:
             isostring (str): A datetime string in ISO format.
 
         """
-        if type(string) == bytes:
-            string = string.decode(encoding)
-        elif type(string) == np.bytes_:
-            string = string.tobytes().decode(encoding)
+        if type(dt_string) == bytes:
+            dt_string = dt_string.decode(self.info['encoding'])
+        elif type(dt_string) == np.bytes_:
+            dt_string = dt_string.tobytes().decode(self.info['encoding'])
 
         try:
-            dt = datetime.strptime(string, self.pattern)
+            dt = datetime.strptime(dt_string, self._pattern)
         except:
-            self._setpattern(string)
-            dt = datetime.strptime(string, self.pattern)
+            self._setpattern(dt_string)
+            dt = datetime.strptime(dt_string, self._pattern)
 
-        if self.cutoffsec:
+        if self.info['cutoffsec']:
             return dt.strftime(ISO_8601)[:-5] + '00000'
         else:
             return dt.strftime(ISO_8601)
 
-    def _setpattern(self, string):
+    def _setpattern(self, dt_string):
         for pattern in PATTERNS:
             try:
-                dt = datetime.strptime(string, pattern)
-                self.pattern = pattern
+                dt = datetime.strptime(dt_string, pattern)
+                self._pattern = pattern
                 break
             except:
                 continue
