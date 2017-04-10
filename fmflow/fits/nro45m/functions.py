dfc974a9a0412795bba7f902cbeec9a6e8dd56f1
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-04-10 21:36:15 +0900

    Add function 'fromnro45m'

--- fmflow/fits/nro45m/functions.py
+++ fmflow/fits/nro45m/functions.py
@@ -1,1 +1,69 @@
 # coding: utf-8
+
+# imported items
+__all__ = ['fromnro45m']
+
+# standard library
+import json
+import os
+import re
+from collections import OrderedDict
+from pkgutil import get_data
+
+# dependent packages
+import yaml
+import fmflow as fm
+import numpy as np
+from astropy import constants
+from astropy import coordinates
+from astropy import units as u
+from astropy.io import fits
+
+# constants
+C           = constants.c.value # spped of light in vacuum
+D_ASTE      = (45.0 * u.m).value # diameter of the ASTE
+EFF_8257D   = 0.92 # exposure / interval time of Agilent 8257D
+IGNORED_KEY = '^reserve' # reserved[1|4|8]
+LAT_NRO45m  = coordinates.Angle('+35d56m40.9s').deg # latitude of the NRO45m
+
+
+# functions
+def fromnro45m(fmlolog, backendlog, antennalog=None, byteorder='<'):
+    """Read logging data of NRO45m and merge them into a FITS object.
+
+    Args:
+        fmlolog (str): File name of FMLO logging.
+        backendlog (str): File name of backend logging.
+        antennalog (str): File name of antenna logging (optional).
+        byteorder (str): format string that represents byte order
+            of the backendlog. Default is '<' (little-endian).
+            If the data in the returned FITS seems to be wrong,
+            try to spacify '>' (big-endian).
+
+    Returns:
+        hdus (HDUlist): HDU list containing the merged data.
+
+    """
+    # PRIMARY HDU
+    hdus = fits.HDUList()
+    hdus.append(fits.PrimaryHDU())
+
+    # FMLOINFO HDU
+    hdus.append(read_fmlolog(fmlolog))
+
+    # BACKEND and OBSINFO HDUs
+    backend = check_backend(backendlog, byteorder)
+
+    if backend == b'SAM45':
+        hdus.append(read_backendlog_sam45(backendlog, byteorder))
+        hdus.insert(1, make_obsinfo_sam45(hdus))
+    else:
+        raise fm.utils.FMFlowError('invalid logging type')
+
+    # ANTENNA HDU (if any)
+    if antennalog is not None:
+        hdus.append(read_antennalog(antennalog))
+
+    return hdus
+
+
