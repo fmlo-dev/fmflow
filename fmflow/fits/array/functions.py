c91223d739b8b75cfc13916d73931633ac88cf19
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-05-17 22:03:13 +0900

    Add function 'getarray'

--- fmflow/fits/array/functions.py
+++ fmflow/fits/array/functions.py
@@ -1,5 +1,83 @@
 # coding: utf-8
 
+# imported items
+__all__ = ['getarray']
+
+# standard library
+import os
+from datetime import timedelta
+from functools import reduce
+
+# dependent packages¬
+import numpy as np
+import fmflow as fm
+from astropy.io import fits
+
+
+# functions
+def getarray(fitsname, arrayid, scantype, offsetsec=0.0):
+    """Create a modulated array from a FMFITS.
+
+    Args:
+        fitsname (str): File name of a FMFITS.
+        arrayid (str): An array ID with which the output fmarray is created.
+        scantype (str): A scan type with which the output fmarray is created.
+        offsetsec (float, optional): A float value of FM offset time in units of sec.
+
+    Returns:
+        array (xarray.DataArray): A modulated array of the spacified `arrayid` and `scantype`.
+
+    """
+    with fits.open(os.path.expanduser(fitsname)) as f:
+        # fits data
+        fmlo = f['fmlolog'].data
+        be   = f['backend'].data
+        if 'antenna' in f:
+            ant = f['antenna'].data
+
+        # data other than ON (e.g. R, SKY, ZERO)
+        if not scantype == 'ON':
+            flag_be = (be['arrayid']==arrayid) & (be['scantype']==scantype)
+            return np.squeeze(be['arraydata'][flag_be])
+
+        # fmarray data
+        t_com, flag_fmlo, flag_be, flag_ant = flag(f, arrayid, scantype, offsetsec)
+        data = np.squeeze(be['arraydata'][flag_be])
+
+        # fmarray coords
+        info = f['obsinfo'].data
+        flag_info = (info['arrayid']==arrayid)
+        info = dict(zip(info.names, info[flag_info][0]))
+        info.update(f['obsinfo'].header)
+
+        tcoords = {'time': t_com}
+        chcoords = {}
+        ptcoords = {'status': 'MODULATED'}
+
+        ## fmch, vrad
+        fmfreq = fmlo['FMFREQ'][flag_fmlo]
+        vrad = fmlo['VRAD'][flag_fmlo]
+        fmch = (fmfreq / info['chanwidth']).astype(int)
+        tcoords.update({'fmch': fmch, 'vrad': vrad})
+
+        ## x/yrel, x/yref (if any)
+        if 'antenna' in f:
+            xref, yref = info['RA'], info['DEC']
+            xrel = ant['RA'][flag_ant] - xref
+            yrel = ant['DEC'][flag_ant] - yref
+            tcoords.update({'xrel': xrel, 'yrel': yrel})
+            ptcoords.update({'xref': xref, 'yref': yref, 'coordsys': 'RADEC'})
+
+        ## fsig, fimg
+        step = info['chanwidth']
+        start = info['restfreq'] - info['restchan']*step
+        end = start + info['numchan']*step
+        fsig = np.arange(start, end, step)
+        fimg = fsig[::-1] - 2*info['intmfreq']
+        chcoords.update({'fsig': fsig, 'fimg': fimg})
+
+        return fm.array(data, tcoords, chcoords, ptcoords)
+
 
 def flag(f, arrayid, scantype, offsetsec=0.0):
     p = fm.utils.DatetimeParser(False)
