078e151a35e20ab01c019e805ea283b7a8483607
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-06-27 16:07:33 +0900

    Add function 'ozonelines'

--- fmflow/models/atmosphere/functions.py
+++ fmflow/models/atmosphere/functions.py
@@ -1,1 +1,15 @@
 # coding: utf-8
+
+# imported items
+__all__ = [
+    'ozonelines',
+]
+
+# dependent packages
+import fmflow as fm
+import numpy as np
+
+
+# functions
+def ozonelines(array):
+    pass
