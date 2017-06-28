bb2169b303b955d3a4d1cbbbad51fccd6e059f92
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-06-28 10:22:37 +0900

    Update function 'ozonelines'

--- fmflow/models/atmosphere/functions.py
+++ fmflow/models/atmosphere/functions.py
@@ -11,5 +11,24 @@ import numpy as np
 
 
 # functions
-def ozonelines(array):
-    pass
+def ozonelines(array, weights=None, fitmode='normal', smooth=50):
+    """
+
+    Args:
+        array (xarray.DataArray):
+        weights (xarray.DataArray):
+        fitmode (str):
+        smooth (int):
+
+    Returns:
+        tb (xarray.DataArray):
+
+    """
+    model = fm.models.OzoneLines(fitmode, smooth)
+    freq = fm.getfreq(array, unit='GHz')
+    spec = fm.getspec(array, weights=weights)
+    vrad = array.vrad.values.mean()
+
+    tb_ = model.fit(freq, spec, vrad)
+    array_ = fm.demodulate(array)
+    return fm.modulate(fm.zeros_like(array_ + tb_))
