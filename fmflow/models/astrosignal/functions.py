ee04af09c7bce103fe1c67ea33ba909b2b85c537
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-07-11 16:02:25 +0900

    Update function 'astrolines'

--- fmflow/models/astrosignal/functions.py
+++ fmflow/models/astrosignal/functions.py
@@ -11,17 +11,29 @@ import numpy as np
 
 
 # functions
-def astrolines(array, weights=None, mode='fit', snr_threshold=10, ch_fwhm=5, ch_smooth=50):
-    model = fm.models.AstroLines(snr_threshold, ch_fwhm, ch_smooth)
+def astrolines(array, weights=None, mode='fit', fit_function='gaussian', snr_threshold=5, ch_smooth=50):
     freq = fm.getfreq(array, unit='GHz').values
     spec = fm.getspec(array, weights=weights).values
+    weight = fm.demodulate(fm.ones_like(array)).sum('t')
+    weight = np.sqrt(weight / weight.max()).values
+
+    model = fm.models.AstroLines(fit_function, snr_threshold, ch_smooth)
 
     if mode == 'fit':
-        tb_ = model.fit(freq, spec)
+        fm.logger.info('mode: fit')
+        fm.logger.info('fit function: {0}'.format(fit_function))
+        fm.logger.info('S/N threshold: {0}'.format(snr_threshold))
+        tb_ = model.fit(freq, spec, weight)
     elif mode == 'smooth':
-        tb_ = model.smooth(freq, spec)
+        fm.logger.info('mode: smooth')
+        fm.logger.info('ch smooth: {0}'.format(ch_smooth))
+        tb_ = model.smooth(spec)
     elif mode == 'raw':
+        fm.logger.info('mode: raw')
         tb_ = spec
+    else:
+        fm.logger.error('invalid mode')
+        raise ValueError(mode)
 
     array_ = fm.demodulate(array)
     return fm.modulate(fm.zeros_like(array_) + tb_)
