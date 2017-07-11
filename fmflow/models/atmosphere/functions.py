f7eef79f0ca3b3df03293d79e0d33a73b12abea7
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-07-11 16:01:57 +0900

    Update function 'atmoslines'

--- fmflow/models/atmosphere/functions.py
+++ fmflow/models/atmosphere/functions.py
@@ -11,15 +11,17 @@ import numpy as np
 
 
 # functions
-def atmoslines(array, weights=None, mode='fit'):
-    model = fm.models.AtmosLines()
+def atmoslines(array, weights=None, mode='fit', ch_tolerance=5):
+    model = fm.models.AtmosLines(ch_tolerance)
     freq = fm.getfreq(array, unit='GHz').values
     spec = fm.getspec(array, weights=weights).values
     vrad = array.vrad.values.mean()
 
     if mode == 'fit':
+        fm.logger.info('mode: fit')
         tb_ = model.fit(freq, spec, vrad)
     elif mode == 'generate':
+        fm.logger.info('mode: generate')
         tb_ = model.generate(freq, vrad)
 
     array_ = fm.demodulate(array)
