59253b3f449d2fb51a83040d7c91c7de37e31018
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-07-11 16:02:14 +0900

    Update class 'AstroLines'

--- fmflow/models/astrosignal/classes.py
+++ fmflow/models/astrosignal/classes.py
@@ -16,39 +16,41 @@ from scipy.optimize import curve_fit
 
 # classes
 class AstroLines(object):
-    def __init__(self, snr_threshold=10, ch_fwhm=5, ch_smooth=50):
+    def __init__(self, fit_function='gaussian', snr_threshold=5, ch_smooth=50):
         self.params = {
+            'fit_function': fit_function,
             'snr_threshold': snr_threshold,
-            'ch_fwhm': ch_fwhm,
             'ch_smooth': ch_smooth,
         }
 
-    def fit(self, freq, spec):
+    def fit(self, freq, spec, weight):
         model = np.zeros_like(spec)
         resid = spec.copy()
 
-        def snr(spec):
-            return spec / fm.utils.mad(spec)
+        def snr(spec, weight):
+            return spec*weight / fm.utils.mad(spec)
 
-        while np.max(snr(resid)) > self.snr_threshold:
-            cent0 = freq[np.argmax(resid)]
-            ampl0 = np.max(resid)
-            fwhm0 = self.ch_fwhm
+        while np.max(snr(resid, weight)) > self.snr_threshold:
+            cent0 = freq[np.argmax(resid*weight)]
+            ampl0 = resid[np.argmax(resid*weight)]
+            fwhm0 = np.diff(freq).mean()
 
             p0 = [cent0, fwhm0, ampl0]
-            bs = ([np.min(freq), 0.0, 0.0], [np.max(freq), 10.0*fwhm0, 10.0*ampl0])
-            popt, pcov = curve_fit(fm.utils.gaussian, freq, resid, p0, bounds=bs)
+            popt, pcov = curve_fit(fm.utils.gaussian, freq, resid, p0)
 
-            model += 0.5 * fm.utils.gaussian(freq, *popt)
-            resid -= 0.5 * fm.utils.gaussian(freq, *popt)
+            model += 0.5 * getattr(fm.utils, self.fit_function)(freq, *popt)
+            resid -= 0.5 * getattr(fm.utils, self.fit_function)(freq, *popt)
 
         return model
 
-    def _smooth(self, freq, spec):
+    def smooth(self, spec):
         return gaussian_filter(spec, self.ch_smooth)
 
     def __getattr__(self, name):
         return self.params[name]
 
     def __repr__(self):
-        return 'AstroLines'
+        return str.format(
+            'AstroLines(fit_function={0}, snr_threshold={1}, ch_smooth={2})',
+            self.fit_function, self.snr_threshold, self.ch_smooth
+        )
