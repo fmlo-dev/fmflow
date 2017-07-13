5e722dc5fc9f4af8de11683597539e98c03ebe15
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-07-13 11:08:01 +0900

    Update function 'full_like'

--- fmflow/array/functions.py
+++ fmflow/array/functions.py
@@ -159,13 +159,17 @@ def ones_like(array, dtype=None, keepmeta=True):
         return fm.ones(array.shape, dtype)
 
 
-def full_like(array, fill_value, dtype=None, keepmeta=True):
+def full_like(array, fill_value, reverse=False, dtype=None, keepmeta=True):
     """Create an array of `fill_value` with the same shape and type as the input array.
 
     Args:
         array (xarray.DataArray): The shape and data-type of it define
             these same attributes of the output array.
-        fill_value (scalar): Fill value.
+        fill_value (scalar or numpy.ndarray): Fill value or array. If broadcast
+            is failed, then try to compute this function in demodulated space.
+        reverse (bool, optional): If True, and if the array is modulated, then
+            the array is reverse-demodulated in the case of computing this
+            function in demodulated space. Default is False.
         dtype (data-type, optional): If spacified, this function overrides
             the data-type of the output array.
         keepmeta (bool, optional): Whether *coords, attrs, and name of the input
@@ -176,9 +180,16 @@ def full_like(array, fill_value, dtype=None, keepmeta=True):
 
     """
     if keepmeta:
-        return xr.full_like(array, dtype)
+        try:
+            return (fm.zeros_like(array) + fill_value).astype(dtype)
+        except ValueError as err:
+            if array.fm.isdemodulated:
+                raise ValueError(err)
+
+            array_ = fm.demodulate(array, reverse)
+            return fm.modulate(array_ + fill_value).astype(dtype)
     else:
-        return fm.full(array.shape, dtype)
+        return fm.full(array.shape, fill_value, dtype)
 
 
 def empty_like(array, dtype=None, keepmeta=True):
