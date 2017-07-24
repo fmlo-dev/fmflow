68ad1f70f3e5755fd6b2ec6f60738c779daf4f1d
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-07-24 14:44:19 +0900

    Update function 'atmoslines'

--- fmflow/models/atmosphere/functions.py
+++ fmflow/models/atmosphere/functions.py
@@ -5,6 +5,9 @@ __all__ = [
     'atmoslines',
 ]
 
+# standard library
+import logging
+
 # dependent packages
 import fmflow as fm
 import numpy as np
@@ -12,17 +15,21 @@ import numpy as np
 
 # functions
 def atmoslines(array, weights=None, mode='fit', ch_tolerance=5):
-    model = fm.models.AtmosLines(ch_tolerance)
+    logger = logging.getLogger('fmflow.models.atmoslines')
+    model = fm.models.AtmosLines(ch_tolerance, logger=logger)
+
     freq = fm.getfreq(array, unit='GHz').values
     spec = fm.getspec(array, weights=weights).values
     vrad = array.vrad.values.mean()
 
     if mode == 'fit':
-        fm.logger.info('mode: fit')
-        tb_ = model.fit(freq, spec, vrad)
+        logger.info('mode: fit')
+        tb = model.fit(freq, spec, vrad)
     elif mode == 'generate':
-        fm.logger.info('mode: generate')
-        tb_ = model.generate(freq, vrad)
+        logger.info('mode: generate')
+        tb = model.generate(freq, vrad)
+    else:
+        fm.logger.error('invalid mode')
+        raise ValueError(mode)
 
-    array_ = fm.demodulate(array)
-    return fm.modulate(fm.zeros_like(array_) + tb_)
+    return fm.full_like(array, tb)
