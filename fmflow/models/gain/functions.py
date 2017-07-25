50598ea45e0fdb9f9132fac60eacafc394b40757
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-07-25 11:45:54 +0900

    Update function 'rfgain'

--- fmflow/models/gain/functions.py
+++ fmflow/models/gain/functions.py
@@ -5,11 +5,18 @@ __all__ = [
     'rfgain',
 ]
 
+# standard library
+from logging import getLogger
+
 # dependent packages
 import fmflow as fm
 import numpy as np
 
 # functions
 def rfgain(ON, ch_smooth=50, convergence=0.01, n_maxiters=100, *, include_logain=False):
-    model = fm.models.RFGain(ch_smooth, convergence, n_maxiters, include_logain)
+    logger = getLogger('fmflow.models.rfgain')
+
+    model = fm.models.RFGain(
+        ch_smooth, convergence, n_maxiters, include_logain, logger=logger
+    )
     return model.fit(ON)
