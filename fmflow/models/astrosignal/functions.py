8d3f55cbd8eb83bf7cb431316270cd0ac20f02c3
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-07-26 23:07:25 +0900

    Update function 'astrolines'

--- fmflow/models/astrosignal/functions.py
+++ fmflow/models/astrosignal/functions.py
@@ -14,32 +14,25 @@ import numpy as np
 
 
 # functions
+@fm.timechunk
 def astrolines(
-        array, weights=None, mode='fit', fit_function='gaussian',
+        array, weights=None, fit_function='gaussian',
         snr_threshold=5, subtraction_gain=0.5
     ):
     logger = getLogger('fmflow.models.atmoslines')
+    logger.info('fit function: {0}'.format(fit_function))
+    logger.info('S/N threshold: {0}'.format(snr_threshold))
+    logger.info('subtracttion gain: {0}'.format(subtraction_gain))
+
+    freq = fm.getfreq(array, unit='GHz').values
+    spec = fm.getspec(array, weights=weights).values
+
+    # normalized rms
+    nrms = fm.demodulate(fm.ones_like(array)).sum('t').values**-0.5
+    nrms /= np.min(rms)
 
     model = fm.models.AstroLines(
         fit_function, snr_threshold, subtraction_gain, logger=logger
     )
-
-    freq = fm.getfreq(array, unit='GHz').values
-    spec = fm.getspec(array, weights=weights).values
-    weight = fm.demodulate(fm.ones_like(array)).sum('t')
-    weight = np.sqrt(weight / weight.max()).values
-
-    if mode == 'fit':
-        logger.info('mode: fit')
-        logger.info('fit function: {0}'.format(fit_function))
-        logger.info('S/N threshold: {0}'.format(snr_threshold))
-        logger.info('subtracttion gain: {0}'.format(subtraction_gain))
-        tb = model.fit(freq, spec, weight)
-    elif mode == 'raw':
-        logger.info('mode: raw')
-        tb = spec
-    else:
-        logger.error('invalid mode')
-        raise ValueError(mode)
-
+    tb = model.fit(freq, spec, nrms)
     return fm.full_like(array, tb)
