6416d58ae9944480e6d4f16f724ceed2384acdfe
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-07-26 20:38:37 +0900

    Add function 'rgain'

--- fmflow/models/gain/functions.py
+++ fmflow/models/gain/functions.py
@@ -3,6 +3,7 @@
 # public items
 __all__ = [
     'ongain',
+    'rgain',
 ]
 
 # standard library
@@ -18,3 +19,10 @@ def ongain(ON, include=['RF', 'LO'], convergence=0.01, n_maxiters=100):
     logger = getLogger('fmflow.models.ongain')
     model = fm.models.ONGain(include, convergence, n_maxiters, logger=logger)
     return model.fit(ON)
+
+
+def rgain(Gon):
+    logger = getLogger('fmflow.models.rgain')
+    iGon = fm.models.ONGain.to_ilogON(Gon)
+    gr = iGon[iGon.fmch==0][0]
+    return fm.zeros_like(Gon[0].drop(Gon.fm.tcoords.keys())) + gr
