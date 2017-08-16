8615034acacf8b4b3ead122dcc159d45d63c5fd4
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-08-16 23:19:23 +0900

    Update __init__.py

--- fmflow/__init__.py
+++ fmflow/__init__.py
@@ -3,11 +3,11 @@
 __version__ = '0.0'
 __author__  = 'snoopython'
 
+from . import utils
 from .array import *
 from .logging import *
 from . import fits
 from . import models
-from . import utils
 
 # default logger
 import logging
