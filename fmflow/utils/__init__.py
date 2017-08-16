8529b683460cdba008ca74fef36154eebcfc3fd6
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-08-16 23:21:55 +0900

    Add import 'miscellaneous'

--- fmflow/utils/__init__.py
+++ fmflow/utils/__init__.py
@@ -4,6 +4,7 @@ from .binary import *
 from .convergence import *
 from .datetime import *
 from .fits import *
+from .miscellaneous import *
 from .multiprocessing import *
 from .ndarray import *
 from .progress import *
@@ -11,6 +12,7 @@ del binary
 del convergence
 del datetime
 del fits
+del miscellaneous
 del multiprocessing
 del ndarray
 del progress
