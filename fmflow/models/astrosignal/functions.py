3540819094b4be4826c3528548a089796e1b575e
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-08-19 01:30:08 +0900

    Update function 'astrolines'

--- fmflow/models/astrosignal/functions.py
+++ fmflow/models/astrosignal/functions.py
@@ -19,20 +19,16 @@ def astrolines(
         array, weights=None, fit_function='gaussian',
         snr_threshold=5, subtraction_gain=0.5
     ):
-    logger = getLogger('fmflow.models.atmoslines')
-    logger.debug('fit function: {0}'.format(fit_function))
-    logger.debug('S/N threshold: {0}'.format(snr_threshold))
-    logger.debug('subtracttion gain: {0}'.format(subtraction_gain))
-
-    freq = fm.getfreq(array, unit='GHz').values
-    spec = fm.getspec(array, weights=weights).values
-
-    # normalized rms
-    nrms = fm.demodulate(fm.ones_like(array)).sum('t').values**-0.5
-    nrms /= np.min(nrms)
-
     model = fm.models.AstroLines(
         fit_function, snr_threshold, subtraction_gain, logger=logger
     )
+
+    logger = getLogger('fmflow.models.atmoslines')
+    logger.debug(model.params)
+
+    freq = fm.getfreq(array, unit='GHz').values
+    spec = fm.getspec(array, weights=weights).values
+    nrms = fm.demodulate(fm.ones_like(array)).sum('t').values**-0.5
+    nrms /= np.min(nrms)
     tb = model.fit(freq, spec, nrms)
     return fm.full_like(array, tb)
