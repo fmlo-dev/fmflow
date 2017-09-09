03fb9b5733457cd0145f278f0433a1ed6a14013a
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-09-09 15:11:00 +0900

    Update function 'astrolines'

--- fmflow/models/astrosignal/functions.py
+++ fmflow/models/astrosignal/functions.py
@@ -17,14 +17,14 @@ import numpy as np
 @fm.chunk('array', 'weights')
 def astrolines(
         array, reverse=False, weights=None, function='gaussian',
-        snr_threshold=5, subtraction_gain=0.5
+        despiking=True, snr_threshold=5, subtraction_gain=0.5
     ):
     params = locals()
     logger = getLogger('fmflow.models.atmoslines')
     logger.debug(params)
 
     model = fm.models.AstroLines(
-        function, snr_threshold, subtraction_gain, logger=logger
+        function, despiking, snr_threshold, subtraction_gain, logger=logger
     )
 
     freq  = fm.getfreq(array, reverse, unit='GHz').values
