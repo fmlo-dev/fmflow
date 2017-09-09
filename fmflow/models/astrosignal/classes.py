0ac9cf1d293cef181baf8bf8f96117256ac885cb
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-09-09 12:27:09 +0900

    Update class 'AstroLines'

--- fmflow/models/astrosignal/classes.py
+++ fmflow/models/astrosignal/classes.py
@@ -19,38 +19,52 @@ warnings.simplefilter('ignore', OptimizeWarning)
 # classes
 class AstroLines(object):
     def __init__(
-            self, fit_function='gaussian', snr_threshold=5,
+            self, function='gaussian', snr_threshold=5,
             subtraction_gain=0.5, *, logger=None
         ):
         self.params = {
-            'fit_function': fit_function,
+            'function': function,
             'snr_threshold': snr_threshold,
             'subtraction_gain': subtraction_gain,
         }
 
         self.logger = logger or fm.logger
 
-    def fit(self, freq, spec, nrms=None):
-        func = getattr(fm.utils, self.fit_function)
+    def fit(self, freq, spec, noise):
+        if self.function == 'none':
+            return spec
+        elif self.function == 'cutoff':
+            model = spec.copy()
 
-        if nrms is None:
-            nrms = np.ones_like(spec)
+            # cutoff
+            mask = (spec/noise) < self.snr_threshold
+            model[mask] = 0.0
 
+            # despike
+            mask = (np.hstack([model[1:], 0])+np.hstack([0, model[:-1]])) == 0
+            model[mask] = 0.0
+
+            return model
+        else:
+            func = getattr(fm.utils, self.function)
+            return self._fit(func, freq, spec, noise)
+
+    def _fit(self, func, freq, spec, noise):
         model = np.zeros_like(spec)
         resid = spec.copy()
 
         def snr(spec):
-            return spec / (fm.utils.mad(spec) * nrms)
+            return spec / noise
 
         fwhm0 = np.mean(np.abs(np.diff(freq)))
         maxsnr = np.max(snr(resid))
         while maxsnr > self.snr_threshold:
-            index = np.argmax(resid/nrms)
+            index = np.argmax(snr(resid))
             cent0, ampl0 = freq[index], resid[index]
             p0 = [cent0, fwhm0, ampl0]
 
             try:
-                popt, pcov = curve_fit(fm.utils.gaussian, freq, resid, p0)
+                popt, pcov = curve_fit(func, freq, resid, p0)
                 model += self.subtraction_gain * func(freq, *popt)
                 resid -= self.subtraction_gain * func(freq, *popt)
                 maxsnr = np.max(snr(resid))
