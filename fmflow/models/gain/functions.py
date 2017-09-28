cdb9b5f5b8dbe9b7f65e8e2d801d821f4dd3ad43
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-09-28 17:19:59 +0900

    Update function 'ongain'

--- fmflow/models/gain/functions.py
+++ fmflow/models/gain/functions.py
@@ -14,9 +14,8 @@ import numpy as np
 
 @fm.chunk('Pon')
 def ongain(Pon, polyorders=[1,2,3], convergence=0.001, n_maxiters=100):
-    params = locals()
     logger = getLogger('fmflow.models.ongain')
-    logger.debug(params)
+    logger.debug({k:v for k,v in locals().items() if k!='logger'})
 
     model = fm.models.ONGain(polyorders, convergence, n_maxiters, logger=logger)
     return model.fit(Pon)
