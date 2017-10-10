4478abbcf49e58ffd0b08dba71517fa92ca37e39
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-10-11 03:24:11 +0900

    Update class 'ONGain'

--- fmflow/models/gain/classes.py
+++ fmflow/models/gain/classes.py
@@ -8,70 +8,36 @@ __all__ = [
 # dependent packages
 import fmflow as fm
 import numpy as np
-from numpy.polynomial.polynomial import polyfit, polyval
+from .. import BaseModel
 from scipy.interpolate import interp1d
+from scipy.signal import savgol_filter
+savgol_filter = fm.numpyfunc(savgol_filter)
+
+# module constants
+i8 = 'i8'
 
 
 # classes
-class ONGain(object):
-    def __init__(self, polyorders=[1,2,3], convergence=0.001, n_maxiters=100, logger=None):
+class ONGain(BaseModel):
+    def __init__(self, window_length=51, polyorder=3, logger=None):
+        super().__init__(logger)
         self.params = {
-            'polyorders': polyorders,
-            'convergence': convergence,
-            'n_maxiters': n_maxiters,
+            'window_length': window_length,
+            'polyorder': polyorder,
         }
-        self.outputs = {}
-        self.logger = logger or fm.logger
 
     def fit(self, Pon):
         logG = np.log10(Pon)
         ilogG = self.to_ilogG(logG)
-
-        # initial arrays
+        ilogG = savgol_filter(ilogG, self.window_length, self.polyorder, axis=0)
         ilogGif = ilogG[ilogG.fmch==0].mean('t')
-        ilogGlo_offset = fm.zeros_like(ilogG)
-        ilogGlo_slopes = fm.zeros_like(ilogG)
-        ilogGlo = ilogGlo_offset + ilogGlo_slopes
-
-        # convergence
-        cv = fm.utils.Convergence(
-            self.convergence, self.n_maxiters, raise_exception=True
-        )
-
-        # algorithm
-        try:
-            while not cv(ilogGlo):
-                self.logger.debug(cv)
-                ilogGlo_offset = self.estimate_offset(ilogG-ilogGif-ilogGlo_slopes)
-                ilogGlo_slopes = self.estimate_slopes(ilogG-ilogGif-ilogGlo_offset)
-                ilogGlo = ilogGlo_offset + ilogGlo_slopes
-        except StopIteration:
-            self.logger.warning('reached maximum iteration')
-
-        # save outputs
-        self.outputs['ilogG'] = ilogG
-        self.outputs['ilogGif'] = ilogGif
-        self.outputs['ilogGlo_offset'] = ilogGlo_offset
-        self.outputs['ilogGlo_slopes'] = ilogGlo_slopes
-        self.outputs['ilogG_residual'] = ilogG - ilogGif - ilogGlo
+        ilogGlo = ilogG - ilogGif
 
         return fm.full_like(logG, 10**self.to_logG(ilogGlo))
 
-    def estimate_offset(self, logG):
-        fmch = logG.fmch.values
-        offset = logG.mean('ch')
-        offset -= offset[fmch==0].values
-        return offset
-
-    def estimate_slopes(self, logG):
-        fmch = logG.fmch.values
-        popt = polyfit(fmch, logG, self.polyorders)
-        slopes = fm.full_like(logG, polyval(fmch, popt).T)
-        return slopes
-
     @staticmethod
     def to_ilogG(logG):
-        bfmch = logG.fmch.values.tobytes()
+        bfmch = logG.fmch.values.astype(i8).tobytes()
         ifmch = np.arange(logG.fmch.min(), logG.fmch.max()+1)
 
         glogG = logG.groupby('fmch').mean('t')
@@ -81,13 +47,7 @@ class ONGain(object):
     @staticmethod
     def to_logG(ilogG):
         ifmch = ilogG.fmch.values
-        fmch = np.fromstring(ilogG.bfmch.values, int)
+        fmch = np.fromstring(ilogG.bfmch.values, i8)
 
         interp = interp1d(ifmch, ilogG, axis=0)
         return fm.array(interp(fmch), {'fmch': fmch})
-
-    def __getattr__(self, name):
-        return self.params[name]
-
-    def __repr__(self):
-        return 'Gain({0})'.format(self.params)
