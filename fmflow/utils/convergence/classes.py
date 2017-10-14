b1c6b7376f088e88811d26d39903297fa59992a3
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-10-14 18:25:55 +0900

    Update class 'Convergence'

--- fmflow/utils/convergence/classes.py
+++ fmflow/utils/convergence/classes.py
@@ -17,16 +17,27 @@ from scipy.stats import gmean
 
 # classes
 class Convergence(object):
-    def __init__(self, threshold=0.01, n_maxiters=100, n_miniters=2,
-            *, reuse_instance=True, raise_exception=False, display_status=True
-        ):
+    def __init__(self, threshold=1e-3, n_maxiters=300, n_miniters=2,
+                 centering=False, reuseable=True, *, raise_exception=False):
+        """Determine a convergence of data by monitoring their variation in iterations.
+
+        Args:
+            threshold (float or list of float): threshold value(s) of data variation.
+            n_maxiters (int): Maximum number of iterations.
+            n_miniters (int): Minimum number of iterations.
+            centering (bool): If True, variation is calculated after centering data.
+            reuseable (bool): If True, status of an instance is reset after converged.
+            raise_exception (bool): If True, StopIteration exception is raised
+                instead of returning True when data are not converged.
+
+        """
         self.params = {
             'threshold': threshold,
             'n_maxiters': n_maxiters,
             'n_miniters': n_miniters,
-            'reuse_instance': reuse_instance,
+            'centering': centering,
+            'reuseable': reuseable,
             'raise_exception': raise_exception,
-            'display_status': display_status,
         }
 
         # threshold list
@@ -38,49 +49,52 @@ class Convergence(object):
         self._set_threshold()
         self._reset_status()
 
+    def __call__(self, data_new):
+        self.n_iters += 1
+        self.data_new, self.data_old = data_new, self.data_new
+
+        if self.n_iters <= min(self.n_miniters, self.n_maxiters):
+            return self._not_converged()
+        elif self.n_iters > self.n_maxiters:
+            return self._converged(self.raise_exception)
+        elif not np.any(self.data_new-self.data_old):
+            return self._converged()
+        elif not np.any(self.data_old):
+            return self._not_converged()
+        elif self._get_variation() <= self._threshold:
+            return self._converged()
+        else:
+            return self._not_converged()
+
     @property
     def status(self):
         try:
-            var_data = str(self.vars_data[-1])
+            var = str(self.variations[-1])
         except IndexError:
-            var_data = None
+            var = None
 
-        try:
-            var_vars = str(self.vars_vars[-1])
-        except IndexError:
-            var_vars = None
-
-        return {'n_iters': self.n_iters, 'var_data': var_data, 'var_vars': var_vars}
-
-    def _variation_data(self):
-        var_data = norm(self.data_new-self.data_old) / norm(self.data_old)
-        var_data = round(Decimal(var_data), self._ndigits)
-        self.vars_data.append(var_data)
-        return var_data
-
-    def _variation_vars(self, n_samples=5):
-        if len(self.vars_data) < n_samples+1:
-            return np.inf
-
-        var_new  = float(self.vars_data[-1])
-        vars_old = np.array(self.vars_data[-n_samples-1:-1], dtype=float)
-        var_vars = np.abs(var_new/(gmean(1+vars_old)-1) - 1)
-        var_vars = round(Decimal(var_vars), 2)
-        self.vars_vars.append(var_vars)
-        return var_vars
+        return {'n_iters': self.n_iters, 'variation': var}
 
     def _converged(self, raise_exception=False):
-        if self.reuse_instance:
+        if self.reuseable:
             self._set_threshold()
             self._reset_status()
 
         if raise_exception:
-            raise StopIteration('reached maximum iteration')
+            message = 'reached maximum iteration'
+            raise StopIteration(message)
         else:
             return True
 
     def _not_converged(self):
         return False
+
+    def _get_variation(self):
+        med = np.median(self.data_old) if self.centering else 0
+        var = norm(self.data_new-self.data_old) / norm(self.data_old-med)
+        var = round(Decimal(var), self._ndigits)
+        self.variations.append(var)
+        return var
 
     def _set_threshold(self):
         try:
@@ -93,33 +107,10 @@ class Convergence(object):
     def _reset_status(self):
         self.n_iters = 0
         self.data_new = None
-        self.vars_data = []
-        self.vars_vars = []
-
-    def __call__(self, data_new):
-        self.n_iters += 1
-        self.data_new, self.data_old = data_new, self.data_new
-
-        if self.n_iters <= self.n_miniters and self.n_maxiters:
-            return self._not_converged()
-        elif self.n_iters > self.n_maxiters:
-            return self._converged(self.raise_exception)
-        elif not np.any(self.data_new-self.data_old):
-            return self._converged()
-        elif not np.any(self.data_old):
-            return self._not_converged()
-        elif self._variation_data() <= self._threshold:
-            return self._converged()
-        elif self._variation_vars() <= 0.05:
-            return self._converged()
-        else:
-            return self._not_converged()
+        self.variations = []
 
     def __getattr__(self, name):
         return self.params[name]
 
     def __repr__(self):
-        if self.display_status:
-            return str(self.status)
-        else:
-            return str(self.params)
+        return str(self.status)
