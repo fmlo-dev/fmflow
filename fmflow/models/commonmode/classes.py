2737df55ef9985fe4dd38a6308e8a0e9fc56ed94
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-10-17 03:08:37 +0900

    Update class 'EMPCA'

--- fmflow/models/commonmode/classes.py
+++ fmflow/models/commonmode/classes.py
@@ -52,7 +52,7 @@ class EMPCA(BaseModel):
         if self.optimize_n:
             model = decomposition.TruncatedSVD(K)
             C = model.fit_transform(X)
-            K_opt = 2 * self._optimize_K(C, 5)
+            K_opt = self._optimize_K(C, 10)
             K = K_opt if K_opt < K else K
 
         # initial arrays
@@ -87,7 +87,7 @@ class EMPCA(BaseModel):
 
         # finally
         if self.optimize_n:
-            K_opt = self._optimize_K(C, 5)
+            K_opt = self._optimize_K(C, 7)
             if K_opt < self.n_components:
                 self.logger.info('optimized n_components: {}'.format(K_opt))
                 C, P = C[:,:K_opt], P[:K_opt]
