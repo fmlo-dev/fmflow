b5dbed77b73e27c2102b05d18749fa66ca5b3d75
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-11-02 17:26:14 +0900

    Update function 'cube'

--- fmflow/core/cube/functions.py
+++ fmflow/core/cube/functions.py
@@ -1,7 +1,11 @@
 # coding: utf-8
 
 # public items
-__all__ = []
+__all__ = [
+    'cube',
+    'tocube',
+    'fromcube',
+]
 
 # dependent packages
 import fmflow as fm
@@ -10,13 +14,17 @@ import xarray as xr
 
 
 # functions
-def cube(data, chcoords=None, ptcoords=None, attrs=None, name=None):
+def cube(data, xcoords=None, ycoords=None, chcoords=None,
+         datacoords=None, scalarcoords=None, attrs=None, name=None):
     """Create a cube as an instance of xarray.DataArray with FM accessor.
 
     Args:
         data (numpy.ndarray): A 3D (channel) array.
+        xcoords (dict, optional): A dictionary of arrays that label x axis.
+        ycoords (dict, optional): A dictionary of arrays that label y axis.
         chcoords (dict, optional): A dictionary of arrays that label channel axis.
-        ptcoords (dict, optional): A dictionary of values that don't label any axes (point-like).
+        datacoords (dict, optional): A dictionary of values that label all axes.
+        scalarcoords (dict, optional): A dictionary of values that don't label any axes.
         attrs (dict, optional): A dictionary of attributes to add to the instance.
         name (str, optional): A string that names the instance.
 
@@ -25,15 +33,24 @@ def cube(data, chcoords=None, ptcoords=None, attrs=None, name=None):
 
     """
     # initialize coords with default values
-    cube = xr.DataArray(data, dims='ch', attrs=attrs, name=name)
+    cube = xr.DataArray(data, dims=('x', 'y', 'ch'), attrs=attrs, name=name)
     cube.fmc._initcoords()
 
     # update coords with input values (if any)
-    if chcoords is not None:
-        cube.coords.update({key: ('ch', val) for key, val in chcoords.items()})
+    if xcoords is not None:
+        cube.fmc.updatecoords(xcoords, 'x')
 
-    if ptcoords is not None:
-        cube.coords.update(ptcoords)
+    if ycoords is not None:
+        cube.fmc.updatecoords(ycoords, 'y')
+
+    if chcoords is not None:
+        cube.fmc.updatecoords(chcoords, 'ch')
+
+    if datacoords is not None:
+        cube.fmc.updatecoords(datacoords, ('x', 'y', 'ch'))
+
+    if scalarcoords is not None:
+        cube.fmc.updatecoords(scalarcoords)
 
     return cube
 
