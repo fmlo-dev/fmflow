5a84f19ba43fd25251cf1919f84b62c69d3b166d
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-11-02 23:21:36 +0900

    Update function 'astrolines'

--- fmflow/models/astrosignal/functions.py
+++ fmflow/models/astrosignal/functions.py
@@ -6,6 +6,7 @@ __all__ = [
 ]
 
 # standard library
+from itertools import product
 from logging import getLogger
 
 # dependent packages
@@ -15,15 +16,16 @@ import numpy as np
 
 # functions
 @fm.chunk('array', 'weights')
-def astrolines(array, weights=None, reverse=False, freqlim=None,
+def astrolines(array, weights=None, reverse=False, mode='spectrum', freqlim=None,
                snr_threshold=10, cutoff_width=3, despiking=True, fit_function=None,
-               subtraction_gain=0.5, convergence=1e-3, n_maxiters=10000):
+               subtraction_gain=0.5, convergence=1e-3, n_maxiters=10000, **kwargs):
     """Model astrolines by cutoff-and-fitting method.
 
     Args:
         array (xarray.DataArray):
         weights (xarray.DataArray, optional):
         reverse (bool, optional):
+        mode (str, optional):
         freqlim (list of float, optional):
         snr_threshold (float, optional):
         cutoff_width (float, optional):
@@ -42,6 +44,18 @@ def astrolines(array, weights=None, reverse=False, freqlim=None,
                                  subtraction_gain, convergence=convergence,
                                  n_maxiters=n_maxiters, logger=logger)
 
-    spec = fm.tospectrum(array, weights, reverse)
-    spec[:] = model.fit(1e-9*spec.freq, spec, spec.noise, freqlim)
-    return fm.fromspectrum(spec, array)
+    if mode.lower() == 'spectrum':
+        spec = fm.tospectrum(array, weights, reverse, **kwargs)
+        freq = 1e-9 * spec.freq
+        spec[:] = model.fit(1e-9*spec.freq, spec, spec.noise, freqlim)
+        return fm.fromspectrum(spec, array)
+    elif mode.lower() == 'cube':
+        cube  = fm.tocube(array, weights, reverse, **kwargs)
+        freq  = 1e-9 * cube.freq
+        noise = cube.noise
+        for x, y in product(range(len(cube.x)), range(len(cube.y))):
+            cube[x, y, :] = model.fit(freq, cube[x, y], noise[x, y], freqlim)
+
+        return fm.fromcube(cube, array)
+    else:
+        raise ValueError(mode)
