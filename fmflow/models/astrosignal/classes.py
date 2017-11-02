83f8a7d225609ca84c62443f023d45f4f1a03495
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-11-02 17:38:27 -0400

    Update class 'AstroLines'

--- fmflow/models/astrosignal/classes.py
+++ fmflow/models/astrosignal/classes.py
@@ -20,14 +20,14 @@ warnings.simplefilter('ignore', OptimizeWarning)
 
 # classes
 class AstroLines(BaseModel):
-    def __init__(self, snr_threshold=10, cutoff_width=3, despiking=True,
-                 fit_function=None, subtraction_gain=0.5, *, convergence=1e-3,
-                 n_maxiters=10000, logger=None):
+    def __init__(self, function='cutoff', despiking=True, snr_threshold=10,
+                 deconvolution_width=3, subtraction_gain=0.5,
+                 *, convergence=1e-3, n_maxiters=10000, logger=None):
         params = {
-            'snr_threshold': snr_threshold,
-            'cutoff_width': cutoff_width,
+            'function': function,
             'despiking': despiking,
-            'fit_function': fit_function,
+            'snr_threshold': snr_threshold,
+            'deconvolution_width': deconvolution_width,
             'subtraction_gain': subtraction_gain,
             'convergence': convergence,
             'n_maxiters': n_maxiters,
@@ -39,25 +39,30 @@ class AstroLines(BaseModel):
         spec  = np.asarray(spec).copy()
         noise = np.asarray(noise).copy()
 
-        # cutoff
-        spec[freq<freqlim[0]] = 0
-        spec[freq>freqlim[1]] = 0
-        spec = self._cutoff(freq, spec, noise)
-
-        # despiking
-        if self.despiking:
-            spec = self._despike(spec, noise)
+        # freq limits
+        if freqlim is not None:
+            spec[freq<freqlim[0]] = 0
+            spec[freq>freqlim[1]] = 0
 
         # fitting
-        if not np.any(spec):
-            return spec
-        elif self.fit_function is None:
-            return spec
+        if self.function is None or not np.any(spec):
+            model = spec.copy()
+        elif self.function == 'cutoff':
+            model = spec.copy()
+            model[spec/noise < self.snr_threshold] = 0
+        elif self.function == 'deconvolution':
+            model = self._deconvolve(freq, spec, noise)
         else:
-            func = getattr(fm.utils, self.fit_function)
-            return self._fit(func, freq, spec, freqlim)
+            func  = getattr(fm.utils, self.function)
+            model = self._funcfit(func, freq, spec, freqlim)
 
-    def _cutoff(self, freq, spec, noise):
+        # despiking (if any)
+        if self.despiking:
+            self._despike(model, noise)
+
+        return model
+
+    def _deconvolve(self, freq, spec, noise):
         model = np.zeros_like(spec)
         resid = spec.copy()
         snr = resid / noise
@@ -71,7 +76,7 @@ class AstroLines(BaseModel):
             xpeak = int(xhalf_r/2 + xhalf_l/2) # update
             left, right = resid[:xpeak+1][::-1], resid[xpeak:] # update
 
-            xlimit = int(self.cutoff_width * (xhalf_r-xhalf_l)/2)
+            xlimit = int(self.deconvolution_width * (xhalf_r-xhalf_l)/2)
             xmin_l = xpeak - np.argmin(left[:xlimit])
             xmin_r = xpeak + np.argmin(right[:xlimit])
             ymin_l = resid[xmin_l]
@@ -87,12 +92,7 @@ class AstroLines(BaseModel):
 
         return model
 
-    def _despike(self, spec, noise):
-        neighbors = np.hstack([spec[1:],0]) + np.hstack([0,spec[:-1]])
-        spec[neighbors/noise < 0.5*self.snr_threshold] = 0
-        return spec
-
-    def _fit(self, func, freq, spec, freqlim):
+    def _funcfit(self, func, freq, spec, freqlim):
         # parameter limits
         if freqlim is None:
             freqlim = freq.min(), freq.max()
@@ -123,3 +123,7 @@ class AstroLines(BaseModel):
             self.logger.warning('reached maximum iteration')
 
         return model
+
+    def _despike(self, model, noise):
+        neighbors = np.hstack([model[1:],0]) + np.hstack([0,model[:-1]])
+        model[neighbors/noise < 0.5*self.snr_threshold] = 0
