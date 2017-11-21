183090445f379e3b2f0e93b8a7f44f6aaf0bdb8a
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-11-22 08:45:55 +0900

    Update function 'atmoslines'

--- fmflow/models/atmosphere/functions.py
+++ fmflow/models/atmosphere/functions.py
@@ -7,6 +7,7 @@ __all__ = [
 ]
 
 # standard library
+from itertools import product
 from logging import getLogger
 
 # dependent packages
@@ -16,31 +17,42 @@ import numpy as np
 
 # functions
 @fm.chunk('array', 'weights')
-def atmoslines(array, weights=None, reverse=False, snr_threshold=10, ch_tolerance=5):
+def atmoslines(array, weights=None, reverse=False, mode='spectrum',
+               snr_threshold=10, ch_tolerance=5, **kwargs):
     logger = getLogger('fmflow.models.atmoslines')
-    logger.debug({k:v for k,v in locals().items() if k!='logger'})
-
     model = fm.models.AtmosLines(snr_threshold, ch_tolerance, logger=logger)
 
-    spec = fm.tospectrum(array, weights, reverse)
-    vrad = array.vrad.values.mean()
-    spec[:] = model.fit(1e-9*spec.freq, spec, spec.noise, vrad)
-    return fm.fromspectrum(spec, array)
+    if mode.lower() == 'spectrum':
+        spec  = fm.tospectrum(array, weights, reverse, **kwargs)
+        freq  = 1e-9 * spec.freq
+        noise = spec.noise
+        vrad  = array.vrad.values.mean()
+        spec[:] = model.fit(freq, spec, noise, vrad)
+        return fm.fromspectrum(spec, array)
+    elif mode.lower() == 'cube':
+        cube  = fm.tocube(array, weights, reverse, **kwargs)
+        spec  = cube.mean(('y', 'x'))
+        freq  = 1e-9 * cube.freq
+        noise = (cube.noise**2).sum(('y', 'x'))**0.5
+        noise /= (~np.isnan(cube)).sum(('y', 'x'))
+        vrad  = array.vrad.values.mean()
+        cube[:] = model.fit(freq, spec, noise, vrad)
+        return fm.fromcube(cube, array)
+    else:
+        raise ValueError(mode)
 
 
 def computeam(array, reverse=False):
     logger = getLogger('fmflow.models.computeam')
-    logger.debug({k:v for k,v in locals().items() if k!='logger'})
-
     model = fm.models.AtmosLines(logger=logger)
 
     # signal sideband
     spec_s = fm.tospectrum(array, reverse=False)
-    spec_s[:] = model.generate(1e-9*spec_s.freq)
+    spec_s[:] = model.generate(1e-9 * spec_s.freq)
 
     # image sideband
     spec_i = fm.tospectrum(array, reverse=True)
-    spec_i[:] = model.generate(1e-9*spec_i.freq)
+    spec_i[:] = model.generate(1e-9 * spec_i.freq)
 
     if reverse:
         return fm.fromspectrum(spec_i, array)
