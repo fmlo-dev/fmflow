48fdc35bcaf8ec75e772798a4bf2db6f0df090ce
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-12-10 15:03:52 +0900

    Add function 'fmgain'

--- fmflow/models/gain/functions.py
+++ fmflow/models/gain/functions.py
@@ -3,6 +3,7 @@
 # public items
 __all__ = [
     'ongain',
+    'fmgain',
 ]
 
 # standard library
@@ -19,3 +20,7 @@ def ongain(Pon, window_length=51, polyorder=3):
     logger = getLogger('fmflow.models.ongain')
     model  = fm.models.ONGain(window_length, polyorder, logger=logger)
     return model.fit(Pon)
+
+
+# temporary
+fmgain = ongain
