c5619fd5bd7117eaa4be64f78dfb324cba8a2bc5
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-09-17 23:47:18 +0900

    Update setup scripts

--- setup.py
+++ setup.py
@@ -1,51 +1,9 @@
 # coding: utf-8
 
-# standard library
+# dependent packages
 from setuptools import setup
 
-# module constants
-REQUIRES = ['astropy',
-            'numba',
-            'numpy',
-            'pyyaml',
-            'scikit-learn',
-            'scipy',
-            'tqdm',
-            'xarray']
 
-PACKAGES = [
-    'fmflow',
-    'fmflow.core',
-    'fmflow.core.array',
-    'fmflow.core.cube',
-    'fmflow.core.spectrum',
-    'fmflow.fits',
-    'fmflow.fits.nro45m',
-    'fmflow.fits.aste',
-    'fmflow.logging',
-    'fmflow.models',
-    'fmflow.models.astrosignal',
-    'fmflow.models.atmosphere',
-    'fmflow.models.commonmode',
-    'fmflow.models.gain',
-    'fmflow.utils',
-    'fmflow.utils.binary',
-    'fmflow.utils.convergence',
-    'fmflow.utils.datetime',
-    'fmflow.utils.fits',
-    'fmflow.utils.misc',
-    'fmflow.utils.ndarray'
-]
-
-# main
-setup(
-    name = 'fmflow',
-    description = 'Data analysis package for FMLO',
-    version = '0.2.6',
-    author = 'astropenguin',
-    author_email = 'taniguchi@a.phys.nagoya-u.ac.jp',
-    url = 'https://github.com/fmlo-dev/fmflow',
-    install_requires = REQUIRES,
-    packages = PACKAGES,
-    package_data = {'fmflow': ['data/*.yaml']},
-)
+# main function
+if __name__ == '__main__':
+    setup()
